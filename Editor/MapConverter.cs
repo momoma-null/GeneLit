ee8f26760683c0597fcad6240a82280aaca18cff
momoma <momoma.creation@gmail.com>
2022-09-22 21:14:47 +0900

    Add Mask Map Converter

---
+++ Editor/MapConverter.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.Object;
+
+namespace MomomaAssets.GeneLit
+{
+    static class MapConverter
+    {
+        static class Shaders
+        {
+            public static readonly Shader standardShader = Shader.Find("Standard");
+            public static readonly Shader standardRoughnessShader = Shader.Find("Autodesk Interactive");
+            public static readonly Shader converterShader = Shader.Find("Hidden/MS_MapConverter");
+        }
+
+        [MenuItem("CONTEXT/Material/Convert to Mask Map", false, 184)]
+        static void ConvertToMaskMap(MenuCommand menuCommand)
+        {
+            if (menuCommand.context is Material material)
+            {
+                if (material.shader == Shaders.standardShader)
+                    ConvertToMaskMap_Internal(material, false);
+                else if (material.shader == Shaders.standardRoughnessShader)
+                    ConvertToMaskMap_Internal(material, true);
+            }
+        }
+
+        static void ConvertToMaskMap_Internal(Material material, bool isRoughness)
+        {
+            var convertMat = new Material(Shaders.converterShader);
+            try
+            {
+                convertMat.CopyPropertiesFromMaterial(material);
+                var ids = convertMat.GetTexturePropertyNameIDs();
+                var width = 0;
+                var height = 0;
+                var path = string.Empty;
+                foreach (var i in ids)
+                {
+                    var tex = convertMat.GetTexture(i);
+                    if (tex != null)
+                    {
+                        width = Mathf.Max(tex.width, width);
+                        height = Mathf.Max(tex.height, height);
+                        if (string.IsNullOrEmpty(path) && AssetDatabase.Contains(tex))
+                        {
+                            path = AssetDatabase.GetAssetPath(tex);
+                        }
+                    }
+                }
+                if (width < 1 || height < 1)
+                    return;
+                if (string.IsNullOrEmpty(path))
+                    path = Application.dataPath;
+                path = Path.Combine(Path.GetDirectoryName(path), $"{material.name}_mask.png");
+                if (isRoughness)
+                    convertMat.SetFloat("_UseRoughnessMap", 1f);
+                var descriptor = new RenderTextureDescriptor(width, height) { sRGB = false };
+                var renderTexture = RenderTexture.GetTemporary(descriptor);
+                var dstTexture = new Texture2D(width, height, TextureFormat.RGBA32, false, false);
+                try
+                {
+                    Graphics.Blit(convertMat.mainTexture, renderTexture, convertMat);
+                    Graphics.SetRenderTarget(renderTexture);
+                    dstTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0, false);
+                    dstTexture.Apply();
+                    Graphics.SetRenderTarget(null);
+                    var bytes = dstTexture.EncodeToPNG();
+                    File.WriteAllBytes(path, bytes);
+                    AssetDatabase.ImportAsset(path);
+                    if (AssetImporter.GetAtPath(path) is TextureImporter importer)
+                    {
+                        importer.sRGBTexture = false;
+                        importer.SaveAndReimport();
+                    }
+                }
+                finally
+                {
+                    RenderTexture.ReleaseTemporary(renderTexture);
+                    DestroyImmediate(dstTexture);
+                }
+            }
+            finally
+            {
+                DestroyImmediate(convertMat);
+            }
+        }
+    }
+}
---
+++ Shaders/Utility/MS_MapConverter.shader
@@ -0,0 +1,57 @@
+﻿Shader "Hidden/MS_MapConverter"
+{
+    Properties
+    {
+        _MainTex ("", 2D) = "white" {}
+        _SpecGlossMap ("", 2D) = "white" {}
+        _MetallicGlossMap ("", 2D) = "white" {}
+        _OcclusionMap ("", 2D) = "white" {}
+        _DetailMask ("", 2D) = "white" {}
+        _SmoothnessTextureChannel ("", Float) = 0
+        _UseRoughnessMap ("", Float) = 0
+    }
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+
+            #include "UnityCG.cginc"
+            #pragma vertex vert
+            #pragma fragment frag
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            UNITY_DECLARE_TEX2D(_MainTex);
+            UNITY_DECLARE_TEX2D(_SpecGlossMap);
+            UNITY_DECLARE_TEX2D(_MetallicGlossMap);
+            UNITY_DECLARE_TEX2D(_OcclusionMap);
+            UNITY_DECLARE_TEX2D(_DetailMask);
+
+            fixed _SmoothnessTextureChannel;
+            fixed _UseRoughnessMap;
+
+            void vert(appdata_img v, out v2f o)
+            {
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.uv = v.texcoord;
+            }
+
+            half4 frag(v2f i) : SV_Target
+            {
+                float r = UNITY_SAMPLE_TEX2D(_SpecGlossMap, i.uv).r;
+                float a = UNITY_SAMPLE_TEX2D(_MainTex, i.uv).a;
+                float2 mg = UNITY_SAMPLE_TEX2D(_MetallicGlossMap, i.uv).ra;
+                float o = UNITY_SAMPLE_TEX2D(_OcclusionMap, i.uv).g;
+                float d = UNITY_SAMPLE_TEX2D(_DetailMask, i.uv).a;
+                float s = _UseRoughnessMap ? (1 - r) : (_SmoothnessTextureChannel ? a : mg.y);
+                return half4(mg.x, o, d, s);
+            }
+            ENDCG
+        }
+    }
+}
